da981ebc919164b5318fd5aa2471e19ae6a3e23c
lfernandes00 <luis_santos_fernandes@hotmail.com>
2023-05-28 17:38:03 +0100

    Create a section file for landing page and move code

--- app/page.tsx
+++ app/page.tsx
@@ -1,61 +1,13 @@
 "use client";
-import { motion } from "framer-motion";
-import { Button } from "antd";
+
+import Landing from "../sections/Landing";
 
 import styles from "../styles/page.module.scss";
 
 export default function Home() {
-  function Hero() {
-    const firstSentence = "Coffe Brand";
-    const lastSentence = "Unique Sensations";
-
-    const spanVariants = {
-      visible: {
-        y: 0,
-        scaleY: 1,
-      },
-      hover: {
-        y: [-1, -2, -2.8, 0.9, 0],
-        scaleY: [1, 1.3, 0.8, 1, 1.2],
-      },
-    };
-
-    return (
-      <div className={styles.heroWrapper}>
-        <div className={styles.landingContent}>
-          <h1>
-            {firstSentence.split("").map((letter, i) => (
-              <motion.span
-                key={i}
-                variants={spanVariants}
-                initial="visible"
-                whileHover="hover"
-              >
-                {letter}
-              </motion.span>
-            ))}
-          </h1>
-          <h2>
-            {lastSentence.split("").map((letter, i) => (
-              <motion.span
-                key={i}
-                variants={spanVariants}
-                initial="visible"
-                whileHover="hover"
-              >
-                {letter}
-              </motion.span>
-            ))}
-          </h2>
-          <Button className={styles.heroBtn}>Find out more</Button>
-        </div>
-      </div>
-    );
-  }
-
   return (
     <>
-      <Hero />
+      <Landing />
     </>
   );
 }
