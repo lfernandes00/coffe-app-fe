adcc0410d7a4d79cc34a0a7e5e8a7a80f859bb9c
lfernandes00 <luis_santos_fernandes@hotmail.com>
2023-05-28 22:12:10 +0100

    Add conditional render for loading; add framer motion exit animation

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,37 @@
 "use client";
 
+import { useEffect, useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
+
+import { PreLoader } from "../components/PreLoader";
 import Landing from "../sections/Landing";
 
 import styles from "../styles/page.module.scss";
 
 export default function Home() {
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 3000);
+  }, []);
+
   return (
     <>
+      <AnimatePresence mode="wait">
+        {isLoading && (
+          <motion.div
+            key="preloader"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ y: "50%", opacity: 0 }}
+            transition={{ duration: 0.2, ease: "easeOut" }}
+          >
+            <PreLoader />
+          </motion.div>
+        )}
+      </AnimatePresence>
       <Landing />
     </>
   );
